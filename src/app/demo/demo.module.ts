405fcfe7b3fa48fdb48fa57c2c061da2107ffa8d
ningguanyi <ningguanyi@haima.me>
2022-02-23 12:09:40 +0800

    添加antv x6 demo

--- src/app/demo/demo.module.ts
+++ src/app/demo/demo.module.ts
@@ -1,16 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ChildDemoComponent } from './child-demo/child-demo.component';
 import { DemoRoutingModule } from './demo-routing.module';
-import { IndexDemoComponent } from './index-demo/index-demo.component';
-import { NgContentDemoComponent } from './ng-content-demo/ng-content-demo.component';
-import { NgContentDemo2Component } from './ng-content-demo2/ng-content-demo2.component';
-import { ViewChildDemoComponent } from './view-child-demo/view-child-demo.component';
-import { NgContentIndexComponent } from './ng-content-index/ng-content-index.component';
+import { DirectiveDemoComponent } from './directive-demo/directive-demo.component';
+import { RainbowDirective } from './directive-demo/rainbow.directive';
 import { HostBingingComponent } from './host-binging/host-binging.component';
 import { HostListenerComponent } from './host-listener/host-listener.component';
-import { DirectiveDemoComponent } from './directive-demo/directive-demo.component';
-import { RainbowDirective } from './rainbow.directive';
+import { IndexDemoComponent } from './index-demo/index-demo.component';
+import { NgContentDemoComponent } from './ng-content-index/ng-content-demo/ng-content-demo.component';
+import { NgContentDemo2Component } from './ng-content-index/ng-content-demo2/ng-content-demo2.component';
+import { NgContentIndexComponent } from './ng-content-index/ng-content-index.component';
+import { ChildDemoComponent } from './view-child-demo/child-demo/child-demo.component';
+import { ViewChildDemoComponent } from './view-child-demo/view-child-demo.component';
+import { DagDemoComponent } from './dag-demo/dag-demo.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,7 @@ import { RainbowDirective } from './rainbow.directive';
     HostListenerComponent,
     DirectiveDemoComponent,
     RainbowDirective,
+    DagDemoComponent,
   ],
   imports: [CommonModule, DemoRoutingModule],
 })
